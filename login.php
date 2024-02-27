09ff695cca61168ec48ceb573d4cab5baf06e1a9
Vishal <89019986+Vishal1720@users.noreply.github.com>
2024-02-27 16:18:32 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php $servername="localhost";
+$user="root";
+$password="";
+$con=mysqli_connect($servername,$user, $password);
+
+if(!$con)
+{
+    die("Error");
+}
+echo "Hello";
+$name=$_POST['name'];
+$pass=$_POST['password'];
+$logval=$con->query("SELECT * FROM `yatra`.`regform` WHERE email='$name' and password='$pass' ");
+
+if($logval)
+{
+    Header("Location:userhomepage.html");
+}
