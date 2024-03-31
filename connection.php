61910e8913f34a67f29b936940d55a83c29d2152
Vishal <vishal198shetty@gmail.com>
2024-03-31 16:24:07 +0530

    added code to start session when it is not created

--- connection.php
+++ connection.php
@@ -9,3 +9,10 @@ if(!$con)
 {
     die("Error");
 }
+if (session_status() === PHP_SESSION_NONE) 
+    {session_start();
+        if(!isset($_SESSION['status']))
+$_SESSION['status']='';//initialising values if they haven't been initialises
+if(!isset($_SESSION['email']))
+$_SESSION['email']=null;
+    }
