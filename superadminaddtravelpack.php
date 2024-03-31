0f978545446d47d7a62fbbc12a26959edb5f397f
Vishal <vishal198shetty@gmail.com>
2024-03-31 19:14:20 +0530

    superadminaddtravelpack.php

---
+++ superadminaddtravelpack.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperAdmin</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php
+    include "connection.php";?>
+    <header id='navbar'><figure><img id='logo' src='yatra.jpg' width='80rem' height='80rem'> 
+        <h4 style='display: inline;'>Yatra</h4></figure>
+        <div id='navlinks'>
+        <a href='superadminhomepage.php' class=''> Add Admin</a>
+        <a href='superadminUsers.php' class=''> Users</a>
+        <a href='' class='highlight'> Add Packages</a>
+        </div>
+        
+     
+</header><?php
+    include "addtravelpackform.php";
+    
+?>
+</body>
+</html>
