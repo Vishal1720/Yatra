241df9704af6ba9207f41f70b793266550869039
Vishal <vishal198shetty@gmail.com>
2024-03-31 16:23:15 +0530

    removed redundant connection code

--- adminlogin.php
+++ adminlogin.php
@@ -1,9 +1,6 @@
 <?php 
 if(isset($_POST['name']) && isset($_POST['password'])) {
-$servername="localhost";
-$user="root";
-$password="";
-$con=mysqli_connect($servername,$user, $password);
+include 'connection.php';
 
 if(!$con)
 {
@@ -12,7 +9,7 @@ if(!$con)
 
 $name=$_POST['name'];
 $pass=$_POST['password'];
-$logval=$con->query("SELECT * FROM `yatra`.`adminregform` WHERE email='$name' and password='$pass' ");
+$logval=$con->query("SELECT * FROM `adminregform` WHERE email='$name' and password='$pass' ");
 
 if($logval->num_rows>=1)
 {
@@ -24,7 +21,7 @@ if($logval->num_rows>=1)
 else{
     $res2=$con->query("Select * from `yatra`.`regform` where  email='$name'");
  if($res2->num_rows==1)
- {echo "<script>alert('password is not correct');window.location.href='adminlogin.php'</script>";}
+ {echo "<script>alert('password is not correct');window.location.href='adminlogin.html'</script>";}
 else{echo "<script>alert(' $name is not registered as an admin');window.location.href='adminlogin.php';</script>";}
      
 }
