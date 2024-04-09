bfe8953f2ff78a06d3a1361e85ee20828292d2b8
Vishal <vishal198shetty@gmail.com>
2024-04-09 12:27:05 +0530

    added navbar

--- adminhomepage.php
+++ adminhomepage.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
+<header id='navbar'><figure><img id='logo' src='yatra.jpg' width='80rem' height='80rem'> 
+        <h4 style='display: inline;'>Yatra</h4></figure>
+        <div id='navlinks'> 
+        <a href='adminUsers.php' > Users</a>
+        <a href='superadminaddtravelpack.php' class='highlight'> Add Packages</a>
+        </div>
+</header>
    <?php
     include 'addtravelpackform.php';
    ?>
