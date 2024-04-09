fb7674f52aa63ee08fff56f3ece61c50b7ba5fa4
Vishal <vishal198shetty@gmail.com>
2024-04-09 12:29:25 +0530

    both user and admin details for superadmin with delete option

--- superadminUsers.php
+++ superadminUsers.php
@@ -8,7 +8,15 @@
 </head>
 <body>
     <?php
-    include "connection.php";?>
+    include "connection.php";
+    if(!isset($userview))$userview = 'User';?>
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+$view=$_POST['view'];
+$userview=$view;
+    }
+    ?>
     <header id='navbar'><figure><img id='logo' src='yatra.jpg' width='80rem' height='80rem'> 
         <h4 style='display: inline;'>Yatra</h4></figure>
         <div id='navlinks'>
@@ -16,11 +24,29 @@
         <a href='' class='highlight'> Users</a>
         <a href='superadminaddtravelpack.php' class='highlight'> Add Packages</a>
         </div>
-        
-     
-</header><?php
-    include "userdetails.php";
-    
-?>
+</header>
+
+<form id="viewform" action="" method="post">
+    <select onchange="submitForm()" style="background-color:black;color:white;text-align:center;font-size:1.3rem;margin-left:89%;margin-right:0px;height:fit-content;" name="view">
+<option <?php if($userview == 'User') echo 'selected'; ?> >User</option>
+<option <?php if($userview == 'Admin') echo 'selected'; ?>>Admin</option> 
+</select>
+</form>
+    <?php
+    if($userview == 'User')
+    {
+        include "userdetails.php";
+    }
+    else if($userview == 'Admin')
+    {
+        include "superadminAdminUsers.php";
+    }
+
+    ?>
+<script>
+    function submitForm(){
+document.getElementById("viewform").submit();
+    }
+    </script>
 </body>
 </html>
