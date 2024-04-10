35acb2bbde6983c9843805d22fb0bac758d3392a
Vishal <vishal198shetty@gmail.com>
2024-04-10 13:27:41 +0530

    Fixed error of login of superadmin

--- superadminlogin.php
+++ superadminlogin.php
@@ -4,18 +4,20 @@ require "connection.php";//stops execution if connection.php is not found
 if(isset($_POST['name']) && isset($_POST['password'])) {
 $name=$_POST['name'];
 $pass=$_POST['password'];
-$logval=$con->query("SELECT * FROM `yatra`.`regform` WHERE email='$name' and password='$pass' ");
+$logval=$con->query("SELECT * FROM `yatra`.`superadminform` WHERE email='$name' and password='$pass' ");
 
 if($logval->num_rows>=1)
-{session_start();
-    $_SESSION['email']= $name;
+{
+  $_SESSION['email']= $name;
     $_SESSION['status']='superadmin';
     Header("Location:superadminhomepage.php");
 }
 else{
-    $res2=$con->query("Select * from `yatra`.`regform` where  email='$name'");
+    $res2=$con->query("Select * from `yatra`.`superadminform` where  email='$name'");
  if($res2->num_rows==1)
- {echo "<script>alert('password is not correct')</script>";}
+ {
+    echo "<script>alert('password is not correct')</script>";
+}
 else{echo "<script>alert(' $name is not registered');</script>";}
      
 }
