9ca70b17daa4aca4f524183df5314d7558308740
Vishal <vishal198shetty@gmail.com>
2024-04-26 13:11:44 +0530

    fixed issue of images not inserting and used addslashes to allow special characters in string for quotes

--- addtravelpack.php
+++ addtravelpack.php
@@ -1,33 +1,40 @@
 <?php
+
 require 'connection.php';
-$title=$_POST['title'];
-$desc=$_POST['desc'];
-$cover=$_POST['cover'];
+/*I have added addslashes to add slashes to quotes i.e escape special characters
+ so now it allows quotes also and not throw errors*/
+$title=addslashes($_POST['title']);
+$desc=addslashes($_POST['desc']);
+
 $date=$_POST['dt'];
-$pickup=$_POST['pickup'];
-$drop=$_POST['drop'];
+$pickup=addslashes($_POST['pickup']);
+$drop=addslashes($_POST['drop']);
 $cost=$_POST['cost'];
 
-if(isset($title) && isset($desc) && isset ($cover) && isset($date) && isset($pickup) && isset($drop) && isset($cost))
+if(isset($title) && isset($desc)    && isset($date) && isset($pickup) && isset($drop) && isset($cost))
 {
 
-//echo "$title $desc $cover $date $pickup $drop $cost";
-$query="INSERT INTO `tpackages` (`title`, `description`, 
-`cover`, `date`, `pickuplocation`, `droplocation`, `cost`)
- VALUES ('$title','$desc','$cover','$date',
- '$pickup','$drop','$cost') ";
+ if (isset($_FILES["cover"]) && $_FILES["cover"]["error"] == 0) {
+    $cover = addslashes(file_get_contents($_FILES["cover"]["tmp_name"]));
 
- if(!$con->query($query))
+} else {
+    echo "Error"; 
+}
+
+$query="INSERT INTO `tpackages`
+ (`title`, `description`, `cover`, `date`, `pickuplocation`, `droplocation`, `cost`)
+  VALUES ('$title','$desc','$cover','$date','$pickup','$drop','$cost') ";
+$res = $con->query($query);
+
+ if(!$res)
  {
-die("End");
+die('Insertion failed');
  }
+
  if($_SESSION['status'] == 'admin' and isset($_SESSION['email']))
  echo "<script>alert('Inserted Successfully');window.location.href='adminhomepage.php'</script>";
 
  if($_SESSION['status'] == 'superadmin' and isset($_SESSION['email']))
  echo "<script>alert('Inserted Successfully');window.location.href='superadminhomepage.php'</script>";
-
-
-
 
 }
