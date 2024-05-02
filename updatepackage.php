f21eca820f7fcc4c48f273d21d4e7311351df213
Vishal <vishal198shetty@gmail.com>
2024-05-02 10:47:14 +0530

    addslashes added and fixed form issues

--- updatepackage.php
+++ updatepackage.php
@@ -38,50 +38,51 @@ if(isset($_POST['updbtnpack'])){
 }
 </style>
 <?php
-if(isset($tpid,$tpTitle,$tpDesc,$tpPickup,$tpDrop,$tpDate,$tpCost))
-echo "
+if(isset($tpid,$tpTitle,$tpDesc,$tpPickup,$tpDrop,$tpDate,$tpCost)){
+    
+    ?>
 <link rel='stylesheet' href='index.css'>
 <form id='updatepackform' method='post' action='' style='width:50%;margin:auto;margin-bottom:20px;'>
 <h2>Update package</h2>
 <div class='packcontainer'>
 <label class='loglbl'>Title</label>
-<input type='hidden' name='packid' value='$tpid'>
+<input type='hidden' name='packid' value=" <?php echo $tpid; ?>">
         <input required type='text' name='tptitle'
-         value='$tpTitle'; class='logfields' placeholder='Enter title'>
+         value="<?php echo $tpTitle; ?>" class='logfields' placeholder='Enter title'>
 
          <label class='loglbl'>Description</label>
         <input required type='text' name='tpdesc'
-         value='$tpDesc'; class='logfields' placeholder='Enter description'>
+         value=" <?php echo $tpDesc ?>"; class='logfields' placeholder='Enter description'>
 
          <label class='loglbl'>Pickup</label>
          <input required type='text' name='tppickup'
-          value='$tpPickup'; class='logfields' placeholder='Enter pickup location'>
+          value=" <?php echo $tpPickup ?>"; class='logfields' placeholder='Enter pickup location'>
 
           <label class='loglbl'>Drop</label>
          <input required type='text' name='tpdrop'
-          value='$tpDrop'; class='logfields' placeholder='Enter drop location'>
+          value=" <?php echo $tpDrop ?>"; class='logfields' placeholder='Enter drop location'>
 
           <label class='loglbl'>Date</label>
           <input required type='date' name='tpdate'
-           value='$tpDate'; class='logfields' placeholder='Enter date'>
+           value="<?php echo $tpDate; ?>"; class='logfields' placeholder='Enter date'>
           
           
            <label class='loglbl'>Cost</label>
-           <input required type='text' pattern='[0-9]+' title='Enter valid cost' name='tpcost'
-            value='$tpCost'; class='logfields' placeholder='Enter cost'>
-
-            
+           <input required type="text" pattern="[0-9]+" title="Enter valid cost" name="tpcost"
+            value="<?php echo $tpCost; ?>"; class='logfields' placeholder='Enter cost'>
            </div>
            <button type='submit' style='width:70%;margin-left:10%' id='logsubmit' name='updatepackquerybtn' class='logbtn'>Update</button>
 </form>
-";
+<?php } ?>
+
+<?php
 
 if(isset($_POST['updatepackquerybtn']))
 {
-    $newtitle=$_POST['tptitle'];
-    $newdesc=$_POST['tpdesc'];
-    $newpickup=$_POST['tppickup'];
-    $newdrop=$_POST['tpdrop'];
+    $newtitle=addslashes($_POST['tptitle']);
+    $newdesc=addslashes($_POST['tpdesc']);
+    $newpickup=addslashes($_POST['tppickup']);
+    $newdrop=addslashes($_POST['tpdrop']);
     $newdate=$_POST['tpdate'];
     $idforwhere=$_POST['packid'];
     $newcost=$_POST['tpcost'];
