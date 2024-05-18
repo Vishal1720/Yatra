75c416b6e6d24eb5ba5865234d43fd9bfe9168d9
Vishal <vishal198shetty@gmail.com>
2024-05-18 11:56:36 +0530

    php file for submitting feedback

---
+++ submitfeedback.php
@@ -0,0 +1,20 @@
+<?php
+include "connection.php";
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['feedbtn']))
+{
+$uname=$_POST['username'];
+$email=$_POST['email'];
+$message=addslashes($_POST['message']);
+$query="INSERT INTO `feedbackform`(`username`, `email`, `message`)
+ VALUES ('$uname','$email','$message')";
+ $res=$con->query($query);
+ if($res)
+ {
+echo "<script>alert('Feedback Submitted successfully');
+window.location.href='index.html';
+</script>";
+ }
+ else{
+    echo "<script>alert('Error inserting');</script>";
+ }
+}
