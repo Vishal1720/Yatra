52cfeaea07518c4a246f963e5dc331a4c330b7c6
Vishal <vishal198shetty@gmail.com>
2024-06-04 18:53:24 +0530

    new data displayed first, canceled and booked notification color is different

--- notifications.php
+++ notifications.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="index.css">
 <style>
 .notification{
-    background-color: rgba(102, 11, 23, 0.8);
+    
     text-shadow: 1.5px 1.5px 2px black;
     color:white;
     border-radius: 10px;
    
     width:66%;
-    padding:20px;
+    padding:26px;
     
     font-size:1.2rem;
     display:flex;
@@ -19,7 +19,8 @@
 include ("connection.php");
 if(isset($_SESSION['email'],$_SESSION['status']))
 {
-    $query="SELECT * FROM `bookedpackages` WHERE useremail='{$_SESSION['email']}';";
+    
+    $query="SELECT * FROM `bookedpackages` WHERE useremail='{$_SESSION['email']}' order by timeoforder desc;";
     $res=$con->query($query);
     if($res->num_rows == 0)
     {
@@ -30,15 +31,23 @@ if(isset($_SESSION['email'],$_SESSION['status']))
     else{
     while($row=$res->fetch_assoc() )
     {
-        echo "<div class='notification'>";
-        $query2="SELECT * FROM `tpackages` where ID='{$row['packageid']}'";
+        $message="You have successfully booked package";
+        $endmsg="";
+        $bg='background-color: rgba(11, 102, 223, 0.6);';
+        if($row['status']=='canceled'){
+        $message="Unfortunately your package";
+        $endmsg=" has been canceled by an admin or an employee";  
+        $bg="background-color:rgba(102, 11, 23, 0.8);";  
+    }
+        echo "<div class='notification' style='".$bg."'>";
+        $query2="SELECT * FROM `tpackages` where ID='{$row['packageid']}' ";
         $res2=$con->query($query2);
         $row2=$res2->fetch_assoc();
 
-        echo "You have successfully booked package {$row2['title']} 
+        echo "$message  {$row2['title']} 
         from {$row2['pickuplocation']} to {$row2['droplocation']} scheduled on
          date {$row2['date']} for
-        {$row['people']} persons with total cost of {$row['totalcost']}
+        {$row['people']} persons with total cost of {$row['totalcost']}$endmsg
         <strong>Date and time of Booking:</strong>{$row['timeoforder']}<br>";
         echo "</div>";
 
