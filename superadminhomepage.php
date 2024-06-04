2367a273ed43131c955f691a14fd1ceffdbbde3b
Vishal <vishal198shetty@gmail.com>
2024-06-04 17:51:34 +0530

    changed Superadmin to admin

--- superadminhomepage.php
+++ superadminhomepage.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SuperAdmin</title>
+    <title>Admin</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <?php
     include "connection.php";
     include 'logoutfield.php';?>
-    <header id='navbar'><figure><img id='logo' src='yatra.jpg' width='80rem' height='80rem'> 
+    <header id='navbar'><figure>
+        <img id='logo' src='yatra.jpg' width='80rem' height='80rem'> 
         <h4 style='display: inline;'>Yatra</h4></figure>
         <div id='navlinks'>
-        <a href='' class='highlight'> Add Admin</a>
+        <a href='' class='highlight'> Add Emp</a>
         <a href='superadminUsers.php' > Users</a>
         <a href='superadminaddtravelpack.php' > Add Packages</a>
         </div>
